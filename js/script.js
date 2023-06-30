2ecfa22cc23fe3f3adf925bb54526fc1031d2556
MikeI11 <ivonuk947378@gmail.com>
2023-06-30 23:28:21 +0300

    how to use conditions

--- js/script.js
+++ js/script.js
@@ -20,3 +20,28 @@ console.log(personalMovieDB);
 console.log(personalMovieDB);
 
 
+
+// if (num < 49) {
+// 	console.log("Error");
+// } else if (num > 100) {
+// 	console.log("Too much");
+// } else {
+// 	console.log("Ok!");
+// }
+// (num == 50) ? console.log("Ok") : console.log("Error");
+
+const num = 51;
+switch (num) {
+case 49:
+	console.log("Wrong");
+	break;
+case 100:
+	console.log("Wrong");
+	break;
+case 50:
+	console.log("Write");
+	break;
+default:
+	console.log("Not this time");
+	break;
+}
